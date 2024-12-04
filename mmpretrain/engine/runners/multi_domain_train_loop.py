21768367af3507292205dd9353bc29adc28c9e63
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2024-12-04 13:37:52 +0700

    Normalization coefficients

--- mmpretrain/engine/runners/multi_domain_train_loop.py
+++ mmpretrain/engine/runners/multi_domain_train_loop.py
@@ -6,6 +6,8 @@ from typing import Dict, List, Optional, Sequence, Tuple, Union
 from torch.utils.data import DataLoader
 import torch
 
+from itertools import chain
+
 
 
 # Customized validation loop
@@ -20,6 +22,7 @@ class MultiDomainTrainLoop(EpochBasedTrainLoop):
                 dataloader1: Union[DataLoader, Dict] = None,
                 val_begin: int = 1, 
                 val_interval: int = 1, 
+                norm_coefficient: bool = False,
                 dynamic_intervals: Optional[List[Tuple[int, int]]] = None) -> None:
     
     super().__init__(runner, dataloader, max_epochs, val_begin, val_interval, dynamic_intervals)
@@ -27,6 +30,8 @@ class MultiDomainTrainLoop(EpochBasedTrainLoop):
     self.n_domains = len(dataloaders_multi_domain)
     self.dataloaders = [] #List of all dataloaders
     self.min_dataloader_len = float('inf')
+
+    self.norm_coefficient = norm_coefficient
 
     #build dataloaders
     for dataloader in dataloaders_multi_domain:
@@ -38,83 +43,80 @@ class MultiDomainTrainLoop(EpochBasedTrainLoop):
     for dataloader in self.dataloaders:
       if len(dataloader) < self.min_dataloader_len:
         self.min_dataloader_len = len(dataloader)
-    
-    # print("TYPE OF DATALOADER:")
-    
-    # print("Min numbers of iteration: ", self.min_dataloader_len)
+  
 
   def run_epoch(self) -> None:
-        """Iterate one epoch."""
-        self.runner.call_hook('before_train_epoch')
-        self.runner.model.train()
 
-        # print("Length of dataloaders:", len(self.dataloader), len(self.dataloader1))
+        if self.norm_coefficient:
+          self.normalization_coefficients()
+          self.runner.call_hook('after_train_epoch')
+          self._epoch += 1
 
+        # """Iterate one epoch."""
+        # self.runner.call_hook('before_train_epoch')
+        # self.runner.model.train()
 
-        #dataloader --> iterable dataloader
-        dataloaders_iter = []
-        for dataloader in self.dataloaders:
-           dataloaders_iter.append(iter(dataloader))
+        # #dataloader --> iterable dataloader
+        # dataloaders_iter = []
+        # for dataloader in self.dataloaders:
+        #    dataloaders_iter.append(iter(dataloader))
         
-        #for one epoch, number of iterations is defined by the smallest dataloader
-        for idx in range(self.min_dataloader_len):
-          data = []
-          for idx_domain in range(self.n_domains):
-            data.append(next(dataloaders_iter[idx_domain]))
+        # #for one epoch, number of iterations is defined by the smallest dataloader
+        # for idx in range(self.min_dataloader_len):
+        #   data = []
+        #   for idx_domain in range(self.n_domains):
+        #     data.append(next(dataloaders_iter[idx_domain]))
 
-          #data_batch['inputs']: tensor, shape [D*B, C, H, w], first dim in domain order
-          #data_batch[data_samples]: list, in domain order, e.g. [d1, d2, d3]
-          data_batch = {}
-          data_batch['inputs'] = torch.cat([d['inputs'] for d in data], dim=0)
-          data_batch['data_samples'] = []
-          for d in data:
-            data_batch['data_samples'].extend(d['data_samples'])
-          
-          # print("Finishing looping over all dataloader!")
-          # print(len(data))
-          # print(type(data[0]['data_samples']))
-
-          # print(data_batch['inputs'].shape)
-
-          # print(len(data_batch['data_samples']))
-
-          self.run_iter(idx, data_batch)
-          # print("DONE ONE ITER WITH MULTIPLE DATALOADERS")
-
-
-        # for idx, (data_batch1, data_batch2) in enumerate(zip(self.dataloader, self.dataloader1)):
-          
+        #   #data_batch['inputs']: tensor, shape [D*B, C, H, w], first dim in domain order
+        #   #data_batch[data_samples]: list, in domain order, e.g. [d1, d2, d3]
         #   data_batch = {}
-        #   #data_batch['inputs'] = data_batch1['inputs'] + data_batch2['inputs']
-        #   data_batch['inputs'] = torch.cat((data_batch1['inputs'], data_batch2['inputs']), dim= 0)
-        #   data_batch['inputs'] = torch.cat((data_batch1['inputs'],), dim= 0)
-
-        #   #Concatenate data from multiple dataloader
-        #   data_batch['data_samples'] = data_batch1['data_samples'] + data_batch2['data_samples']
-        #   data_batch['data_samples'] = data_batch1['data_samples']
-
-        #   print("Shape of concatenated data:", data_batch['inputs'].shape)
-        #   print("Length of data samples: ", len(data_batch['data_samples']))
-        #   print("--------------------------")
-        #   print("TYPE OF DATA SAMPLES: ", type(data_batch1['data_samples']))
-
-        #   #Input to model to process
+        #   data_batch['inputs'] = torch.cat([d['inputs'] for d in data], dim=0)
+        #   data_batch['data_samples'] = []
+        #   for d in data:
+        #     data_batch['data_samples'].extend(d['data_samples'])
+  
         #   self.run_iter(idx, data_batch)
-        #   print("Done one iter with concatenated inputs")
-
-        #   break
-
-
-        # for idx, data_batch in enumerate(self.dataloader1):
-        #     self.run_iter(idx, data_batch)
-        #     print(type(data_batch['inputs']))
-        #     print("List of first dataloader could be input to model")
-        #     break
 
         # -------------
         # Original EpochBasedTrainLoop
         # -------------
         self.runner.call_hook('after_train_epoch')
         self._epoch += 1
+
+  def normalization_coefficients(self):
+
+    dataloader_all = chain(dataloader for dataloader in self.dataloaders)
+
+    # Initialize variables
+    total_sum = torch.zeros(3)
+    total_sum_squared = torch.zeros(3)
+    num_samples = 0
+    num_pixels = 0
+
+    #print("LENGHT OF DATALODER:", len(dataloader_all))
+
+    for data in dataloader_all:  # Loop over the dataset
+        # Batch size, channels, height, width
+
+        images = torch.cat([d['inputs'] for d in data], dim=0)
+        batch_size, channels, height, width = images.shape
+
+        # Update pixel count
+        num_samples += batch_size
+        num_pixels += batch_size * height * width
+
+
+        # Sum and squared sum
+        total_sum += images.sum([0, 2, 3])  # Sum over batch, height, width
+        total_sum_squared += (images ** 2).sum([0, 2, 3])
+
+    # Calculate mean and std
+    mean = total_sum / num_pixels
+    std = (total_sum_squared / num_pixels - mean ** 2).sqrt()
+
+    print("Total number of samples:", num_samples)
+
+    print(f"Mean: {mean}")
+    print(f"Std: {std}")
 
   
--- mmpretrain/models/necks/domain_fusion_neck.py
+++ mmpretrain/models/necks/domain_fusion_neck.py
@@ -3,25 +3,106 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from mmpretrain.registry import MODELS
+from torch_geometric.nn import GCNConv
+from mmengine.model import BaseModule
+from .gap import GlobalAveragePooling
+
+
+import torch
 
 
 @MODELS.register_module()
-class MultiDomainInformationFusion(nn.Module):
+class MultiDomainInformationFusion(BaseModule):
+    '''
+    NOTE:
+        Also need to customize classifier to input labels to neck.
+        Inherit from classifier.image.ImageClassifier class
+        Replace in ImageClassifier: 
+            self.neck(x) --> self.neck(x, datasamples); 
+            extract_feat(inputs) --> extract_feat(inputs, data_samples)
+    '''
     def __init__(self, input_dim, hidden_dim, output_dim):
-        super(MultiDomainInformationFusion, self).__init__()
-        # self.conv1 = GCNConv(input_dim, hidden_dim)  # First GCN layer
-        # self.conv2 = GCNConv(hidden_dim, output_dim)  # Second GCN layer
 
-        print("AAAAAAAAAAAAAAAAAAAAAAA")
-    def forward(self, x, edge_index):
+        super(MultiDomainInformationFusion, self).__init__()
+
+        self.gap = GlobalAveragePooling()
+
+        self.gcn_conv1 = GCNConv(input_dim, hidden_dim)  # First GCN layer
+        self.gcn_conv2 = GCNConv(hidden_dim, output_dim)  # Second GCN layer
+
+        '''
+        self.agent_nodes: Tensor, shape (n_domains, n_agent_nodes = n_classes)
+        self.batch_size: information to know which samples are from which domain
+        self.wieght_head: Conv 1x1, head to get contribution weight for agent nodes
+
+        '''
+        
+
+    def forward(self, instance_feat: torch.Tensor, 
+                kn_graph: torch.Tensor,
+                data_samples = None):
         """
+        Input:
+            instance_feat: list of features from multiple levels, default: last layer backbone
         Args:
-            x: Node feature matrix (num_nodes x input_dim)
-            edge_index: Graph connectivity (2 x num_edges)
-                        Each column represents an edge (source, target).
+            instance_feat: Node feature matrix (num_nodes instance_feat input_dim)
+            kn_graph: adjacency matrix, either node --> node or sparse matrix. Fixed
+        Procedure: 
+            Calculate current agent nodes, function calculate_agent_node()
+            Update agent nodes by EMA, function ema()
+            Average pooling instances features to shape: (batch_size, feature_dim, 1, 1)
+            Concatenate instances features with agent nodes feature
+            
+            Input to GCN: self.gcn_conv1 and gcn_conv2
+        
+        Return:
+            Information-fused features (without agent node features)
         """
-        x = self.conv1(x, edge_index)  # Apply first GCN layer
-        x = F.relu(x)                 # Non-linear activation
-        x = self.conv2(x, edge_index)  # Apply second GCN layer
-        print("SHAPE OF OUTPUT OF GCN MULTI DOMAIN FUSION:", x.shape)
-        return x
+
+        print("TYPE OF INPUT GCN LAYER:", type(instance_feat))
+        print('LENGTH OF X:', len(instance_feat))
+        print(type(instance_feat[0]))
+        print('SHAPE OF INPUT:', instance_feat[0].shape)
+        instance_feat = instance_feat[0]
+
+        instance_feat = self.gap(instance_feat)
+        instance_feat = instance_feat.view(instance_feat.size(0), -1)
+        print("SHAPE AFTER POOLING:", instance_feat.shape)
+ 
+        kn_graph = torch.tensor([[0],
+                           [1]], dtype=torch.long)
+                
+        instance_feat = self.gcn_conv1(instance_feat, kn_graph)  # Apply first GCN layer
+        instance_feat = self.gcn_conv2(instance_feat, kn_graph)  # Apply second GCN layer
+
+        print("SHAPE AFTER AVERAGE POOLING:", instance_feat.shape)
+        
+        #Update agents node 
+        
+        print("SHAPE OF OUTPUT OF GCN MULTI DOMAIN FUSION:", instance_feat.shape)
+        return tuple([instance_feat])
+    
+    def agent_feat(instance_feat: torch.Tensor):
+        '''
+        input: instance features 
+        output: agent nodes for current iteration
+
+        Calculate weighted agent nodes by classes
+
+        instance_feat --> weight_head --> instance weight
+        '''
+        pass
+
+    def ema():
+        '''
+        Update agent nodes by EMA
+        '''
+        pass
+
+    def build_matrix():
+        '''
+        Update ajacency matrix
+        '''
+        pass
+    
+        
