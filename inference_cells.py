1b8a3e002ccd05464a4cb0ac9d2196827d8a77be
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2025-03-31 11:42:03 +0700

    inference

--- inference_cells.py
+++ inference_cells.py
@@ -11,6 +11,8 @@ parser = argparse.ArgumentParser(description="Parser for classification")
 parser.add_argument("--model", type=str, help="path to config file")
 parser.add_argument("--pretrained", type=str, help="path to model pretrained (trained parameteres) pt file")
 parser.add_argument("--inputs", type=str, help="path to image or images folder")
+parser.add_argument("--detection_sve_dir", type=str, help="path to image or images folder")
+
 
 args = parser.parse_args()
 
@@ -40,3 +42,29 @@ classification_results = inferencer(inputs = input_images,
 #          9.3389821e-01, 6.5715335e-02, 2.1869126e-04], dtype=float32),
 #   'pred_label': 4,
 #   'pred_score': 0.9338982105255127}]
+
+#----------
+# DETECTION RSULT
+# ---------
+
+txt_result_dir = os.path.join(args.detection_save_dir, "labels")
+txt_file_list = [f for f in os.listdir(txt_result_dir) if os.path.isfile(os.path.join(txt_result_dir, f))]
+
+for txt_file in txt_file_list:
+    result_file = os.path.join(txt_result_dir, txt_file)
+    
+    refined_result = os.path.join(txt_result_dir, txt_file[:-3] + '_refined.txt')
+    refined_result_list = []
+
+    with open(result_file, "r") as file:
+        lines = file.readlines()
+    
+    for i, line in enumerate(lines):
+        parts = line.strip().split()
+        class_name, x_center, y_center, w, h = parts[0], float(parts[1]), float(parts[2]), float(parts[3]), float(parts[4])
+        refined_class_name = classification_results[i]['pred_label']
+        refined_result_list.append('{} {} {} {} {}\n'.format(refined_class_name, x_center, y_center, w, h))
+
+    with open(refined_result, "w") as refined_file:
+        for line in refined_result_list:
+           refined_file.write(line)
