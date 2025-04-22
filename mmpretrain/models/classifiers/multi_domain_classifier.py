2a50c55d910d678338a224bf8ebaf427c586188e
khanhtq2101 <khanh.tq190085@sis.hust.edu.vn>
2025-04-22 16:52:01 +0700

    inference

--- mmpretrain/models/classifiers/multi_domain_classifier.py
+++ mmpretrain/models/classifiers/multi_domain_classifier.py
@@ -21,7 +21,7 @@ class MultiDomainClassifier(ImageClassifier):
                  train_cfg: Optional[dict] = None,
                  data_preprocessor: Optional[dict] = None,
                  init_cfg: Optional[dict] = None,
-                 domain_idx: int = 1 # IML Malaria
+                 domain_idx: int = 2 # IML Malaria
                  ):
         
         super().__init__(backbone, neck, head,
